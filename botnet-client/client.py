33414076911731e75170cb1c60564969eeee522d
Juan Castro <jecastro2@uc.cl>
2018-09-04 00:00:22 -0300

    bruteforce half done

--- botnet-client/client.py
+++ botnet-client/client.py
@@ -2,7 +2,8 @@ from getpass import getuser
 from platform import platform
 import requests
 import uuid
-import hashlib
+from cracker import Cracker
+import time
 
 class Client:
   URL = 'http://localhost:8000'
@@ -11,30 +12,16 @@ class Client:
     self.os = platform()
     self.user = getuser()
     self._uuid = uuid.uuid4()
+    self.cracker = Cracker()
     self.task_id = -1
+
+  def __str__(self):
+    return '{} {}'.format(self.os, self.user)
 
   def get_headers(self):
     return {
       'Authorization': 'Token {}'.format(self._uuid),
     }
-
-  def brute_force_cracking(self, hash_to_crack, algorithm, char_set):
-    if algorithm == 'md5':
-      hash_algorithm = hashlib.md5
-    else:
-      hash_algorithm = hashlib.sha1
-    hash_object = hash_algorithm(b'hola').hexdigest()
-    if hash_object == hash_to_crack:
-      print(True)
-    print(False)
-
-  def parse_command(self, command):
-    command_list = command.split()
-    if command_list[0] == 'bruteforce':
-      hash_to_crack = command_list[command_list.index('-h') + 1]
-      algorithm = command_list[command_list.index('-a') + 1]
-      char_set = command_list[command_list.index('-s') + 1]
-      return self.brute_force_cracking(hash_to_crack, algorithm, char_set)
 
   def authenticate(self):
     url = '{}/auth/'.format(Client.URL)
@@ -50,7 +37,8 @@ class Client:
     if 'task_id' in response and 'command' in response:
       self.task_id = response['task_id']
       self.parse_command(response['command'])
-    self.parse_command('bruteforce -h 925d7518fc597af0e43f5606f9a51512 -a sha1 -s ld -l 8 -p 3/5')
+    # No recibe la tarea, asique aca hay que poner el comando. Por ahora es un solo hash
+    self.parse_command('bruteforce -h 5f249886559f87d1cf8ccaadedd6e11f76c53629 -a sha1 -s lllldd -l 5 -p 1/10')
 
   def deliver_task(self, answer):
     url = '{}/delivery/'.format(Client.URL)
@@ -58,10 +46,16 @@ class Client:
       'task_id': self.task_id,
       'answer': answer,
     })
-    # check this request
 
-  def __str__(self):
-    return '{} {}'.format(self.os, self.user)
+  def parse_command(self, command):
+    command_list = command.split()
+    if command_list[0] == 'bruteforce':
+      hash_to_crack = command_list[command_list.index('-h') + 1]
+      algorithm = command_list[command_list.index('-a') + 1]
+      char_set = command_list[command_list.index('-s') + 1]
+      t = time.time()
+      self.cracker.brute_force_cracking([hash_to_crack], algorithm, char_set)
+      print('TOTAL TIME', time.time() - t)
 
 if __name__ == '__main__':
   client = Client()
