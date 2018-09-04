1a0ddaf650718423fec702403230b27728b765dd
Martín De la Fuente <mdelafuente1@uc.cl>
2018-09-04 17:22:19 -0300

    Changed indentation, search speedup, finishedview request

--- botnet-client/client.py
+++ botnet-client/client.py
@@ -21,6 +21,7 @@ class Client:
         self.user = getuser()
         self._uuid = uuid.uuid4()
         self.task_id = None
+        self.task_thread = None
 
     def get_headers(self):
         return {
@@ -30,45 +31,52 @@ class Client:
     def authenticate(self):
         url = '{}/auth/'.format(Client.URL)
         data = urllib.parse.urlencode({
-          'user': self.user,
-          'uid': self._uuid,
-          'os': self.os,
+            'user': self.user,
+            'uid': self._uuid,
+            'os': self.os,
         }).encode()
         try:
-          req = urllib.request.Request(url, data=data, method='POST')
-          response = urllib.request.urlopen(req)
+            req = urllib.request.Request(url, data=data, method='POST')
+            response = urllib.request.urlopen(req)
         except urllib.error.URLError:
-          return False
+            return False
 
-        if response.status % 200 < 99:
-          return True
+        if response.status == 204:
+            return True
         return False
 
     def get_task(self):
+        print('Talking to server..')
         url = '{}/tasks/'.format(Client.URL)
         req = urllib.request.Request(url, headers=self.get_headers(), method='GET')
         response = urllib.request.urlopen(req)
         response = json.loads(response.read().decode('utf-8'))
         if 'task_id' in response and 'command' in response:
             self.task_id = response['task_id']
-            print('STARTING COMMAND: ', response['command'])
+            print('New task! -> ', response['command'])
             self.parse_command(response['command'])
 
     def deliver_task(self, answer):
         url = '{}/delivery/'.format(Client.URL)
         data = urllib.parse.urlencode({
-          'task_id': self.task_id,
-          'answer': answer,
+            'task_id': self.task_id,
+            'answer': answer,
         }).encode()
         req = urllib.request.Request(url, headers=self.get_headers(), data=data, method='POST')
+        urllib.request.urlopen(req)
+
+    def finished_task(self):
+        url = '{}/finished/'.format(Client.URL)
+        req = urllib.request.Request(url, headers=self.get_headers(), method='GET')
         urllib.request.urlopen(req)
 
     def parse_command(self, command):
         command_list = command.split()
         kwargs = {command_list[i]: command_list[i+1] for i in range(len(command_list)) if command_list[i].startswith("-")}
         if command_list[0] == 'bruteforce':
-            crack_process = Thread(target=self.bruteforce, kwargs=kwargs)
-            crack_process.start()
+            print("Running task in a new thread")
+            self.task_thread = Thread(target=self.bruteforce, kwargs=kwargs)
+            self.task_thread.start()
 
     def bruteforce(self, **kwargs):
         hash_file_url = kwargs.get("-u")
@@ -77,25 +85,35 @@ class Client:
         length = int(kwargs.get("-l"))
         partition = kwargs.get("-p")
 
-        response = urllib.request.urlopen('http://localhost:8080/hashes.txt')
-        hashes = list(response.read().decode('utf8').split('\n'))
+        response = urllib.request.urlopen(hash_file_url)
+        hashes = [hash_.strip() for hash_ in response.read().decode('utf8').strip().split('\n')]
 
         if algorithm.lower() == 'md5':
             hash_algorithm = hashlib.md5
         else:
             hash_algorithm = hashlib.sha1
+
         for word in word_generator(char_set, length, partition):
             hash_object = hash_algorithm(bytes(word, 'utf-8')).hexdigest()
-            for hash_to_crack in hashes:
-              if hash_object == hash_to_crack:
-                  answer = "{}: {}".format(hash_to_crack, word)
-                  print(answer)
-                  self.deliver_task(answer)
+            try:
+                pos = hashes.index(hash_object)
+            except ValueError:
+                continue
+            else:
+                answer = "{}: {}".format(hashes[pos], word)
+                self.deliver_task(answer)
+                print("Match found: " + answer)
+
+        print("Finished task")
+        self.finished_task()
+
 
 if __name__ == '__main__':
     client = Client()
+
     while not client.authenticate():
-      time.sleep(3)
+        time.sleep(30)
+
     while True:
-      client.get_task()
-      time.sleep(10)
+        client.get_task()
+        time.sleep(30)
