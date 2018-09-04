4fceddf9a9c711746048e672d1a09202ae0f279f
Martín De la Fuente <mdelafuente1@uc.cl>
2018-09-04 17:44:59 -0300

    Configured static files

--- botnet-server/botnetserver/settings.py
+++ botnet-server/botnetserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9*8-&aazw1l!@v4vl&bd^#wfx2!(6isvcx)j#ee0h34e4oo89='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["iic2523.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "iic2523.pythonanywhere.com"]
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
